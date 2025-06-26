296280278b511118833272e26071658de2c938c1
you-oops-dev[silent] <you-oops-dev[silent]@users.noreply.github.com>
2025-06-26 03:12:02 +0300

    Edit

--- utils/create_rules_sing-box.sh
+++ utils/create_rules_sing-box.sh
@@ -11,11 +11,15 @@ export SORT_PATH=/tmp/
 cd $HOME_GITHUB/templates/sing-box
 
 # domain
-cat ../dnsmasq/dnsmasq.d/unblock.conf | awk -F / '{print $2}' | sort -T ${SORT_PATH} | uniq | sed '/googlevideo.com/d' | sed '/fastly.net/d' | sed '/discord.gg/d' | sed '/discord.com/d' | sed '/steamserver.net/d' | sed '/themoviedb.org/d' | sed '/voidboost.cc/d' | sed '/jetbrains.com/d' | sed '/intel.com/d' | sed '/archlinux.org/d' | sed '/windows.net/d' | sed '/cloudflareinsights.com/d' | sed '/microsoft.com/d' | sed '/steampowered.com/d' | sed '/akamai.net/d' | sed '/steamcloud-eu-ams.storage.googleapis.com/d' | sed '/steamcontent.com/d' | sed '/steamstatic.com/d' | sed '/akamaized.net/d' | sed '/steamcommunity.com/d' | sed '/steamcloud-eu-fra.storage.googleapis.com/d' | sed '/ggpht.com/d' | sed '/googleapis.com/d' | sed '/googleusercontent.com/d' | sed '/gstatic.com/d' | sed '/returnyoutubedislikeapi.com/d' | sed '/returnyoutubedislike.com/d' | sed '/ajay.app/d' | sed '/ytimg.com/d' | sed '/yting.com/d' | sed '/tmdb.org/d' | sed '/cloudfront.net/d' | sed '/entware.net/d' | sed '/habr.com/d' | sed '/4pda.ru/d' | sed '/4pda.to/d' | sed '/4pda.ws/d' | sed '/torproject.org/d' | sed '/openai.com/d' | sed '/chatgpt.com/d' | sed '/spotify.com/d' | sed '/spotifycdn.com/d' | sed '/scdn.co/d' | sed '/whatsapp.net/d' | sed '/whatsapp.com/d' | sed '/fbcdn.net/d' | sed '/facebook.com/d' | sed '/kinoxa.win/d' | sed '/^youtube.com/d' | sed '/youtube.com/d' | sed '/youtube/d' | sed '/tiktokcdn.com/d' | sed '/githubusercontent.com/d' | sed '/sinema2.top/d' | sed '/megapeer.ru/d' | sed '/pirat.one/d' | sed '/lordfillms.ru/d' | sed '/lordfilm.lu/d' | sed '/byteoversea.net/d' | sed '/google.com/d' | sed '/github.io/d' | sed '/blogspot.com/d' | sed '/musical.ly/d' | sed '/tiktokv.com/d' | sed '/lordserialus.fun/d' | sed '/akamaihd.net/d' | sed '/byteoversea.com/d' | sed '/azotmarket.ru/d' | sed '/kinoteatr.one/d' | sed '/tiktokcdn-eu.com/d' | sed '/ibyteimg.com/d' sort -T ${SORT_PATH} | uniq > ./domain.txt
+cat ../dnsmasq/dnsmasq.d/unblock.conf | awk -F / '{print $2}' | sort -T ${SORT_PATH} | uniq | sed '/googlevideo.com/d' | sed '/fastly.net/d' | sed '/discord.gg/d' | sed '/discord.com/d' | sed '/steamserver.net/d' | sed '/themoviedb.org/d' | sed '/voidboost.cc/d' | sed '/jetbrains.com/d' | sed '/intel.com/d' | sed '/archlinux.org/d' | sed '/windows.net/d' | sed '/cloudflareinsights.com/d' | sed '/microsoft.com/d' | sed '/steampowered.com/d' | sed '/akamai.net/d' | sed '/steamcloud-eu-ams.storage.googleapis.com/d' | sed '/steamcontent.com/d' | sed '/steamstatic.com/d' | sed '/akamaized.net/d' | sed '/steamcommunity.com/d' | sed '/steamcloud-eu-fra.storage.googleapis.com/d' | sed '/ggpht.com/d' | sed '/googleapis.com/d' | sed '/googleusercontent.com/d' | sed '/gstatic.com/d' | sed '/returnyoutubedislikeapi.com/d' | sed '/returnyoutubedislike.com/d' | sed '/ajay.app/d' | sed '/ytimg.com/d' | sed '/yting.com/d' | sed '/tmdb.org/d' | sed '/cloudfront.net/d' | sed '/entware.net/d' | sed '/habr.com/d' | sed '/4pda.ru/d' | sed '/4pda.to/d' | sed '/4pda.ws/d' | sed '/torproject.org/d' | sed '/openai.com/d' | sed '/chatgpt.com/d' | sed '/spotify.com/d' | sed '/spotifycdn.com/d' | sed '/scdn.co/d' | sed '/whatsapp.net/d' | sed '/whatsapp.com/d' | sed '/fbcdn.net/d' | sed '/facebook.com/d' | sed '/kinoxa.win/d' | sed '/^youtube.com/d' | sed '/youtube.com/d' | sed '/youtube/d' | sed '/tiktokcdn.com/d' | sed '/githubusercontent.com/d' | sed '/sinema2.top/d' | sed '/megapeer.ru/d' | sed '/pirat.one/d' | sed '/lordfillms.ru/d' | sed '/lordfilm.lu/d' | sed '/byteoversea.net/d' | sed '/google.com/d' | sed '/github.io/d' | sed '/blogspot.com/d' | sed '/musical.ly/d' | sed '/tiktokv.com/d' | sed '/lordserialus.fun/d' | sed '/akamaihd.net/d' | sed '/byteoversea.com/d' | sed '/azotmarket.ru/d' | sed '/kinoteatr.one/d' | sed '/tiktokcdn-eu.com/d' | sed '/ibyteimg.com/d' | sed '/deepl.com/d' | sed '/x.com/d' | sed '/instagram/d' | sort -T ${SORT_PATH} | uniq > ./domain.txt
 echo "youtube.com
 www.youtube.com
 www.instagram.com
-www.facebook.com" >> ./domain.txt
+www.facebook.com
+deepl.com
+www.deepl.com
+x.com
+www.x.com" >> ./domain.txt
 sort -T ${SORT_PATH} ./domain.txt | uniq | sponge ./domain.txt
 # IP-address 1
 #cat ../ipset/unblock_static.conf.zst | zstd -d | awk '{print $3}' | sed '/:/d' | sort -T ${SORT_PATH} -t. -k1,1n -k2,2n -k3,3n -k4,4n | uniq | sed 's/$/\/32/' > ./ip.txt
@@ -95,7 +99,9 @@ echo ".fastly.net
 .byteoversea.com
 .kinoteatr.one
 .tiktokcdn-eu.com
-.ibyteimg.com" > ./domain_wildcard.txt
+.ibyteimg.com
+.deepl.com
+.x.com" > ./domain_wildcard.txt
 
 
 jq -n \
