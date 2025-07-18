64a204eb5eff0e8586d05776f72f1aaf97935d20
you-oops-dev[silent] <you-oops-dev[silent]@users.noreply.github.com>
2025-07-19 00:07:39 +0300

    Add new suite

--- utils/update-filter-dnsmasq.sh
+++ utils/update-filter-dnsmasq.sh
@@ -566,6 +566,7 @@ sed -i '/^digiseller.ru/d' /tmp/01_unbound_filters.hostname
 sed -i '/^api.digiseller.ru/d' /tmp/01_unbound_filters.hostname
 sed -i '/googlevideo.com/d' /tmp/01_unbound_filters.hostname
 sed -i '/avito.ru/d' /tmp/01_unbound_filters.hostname
+sed -i '/z-lib.id/d' /tmp/01_unbound_filters.hostname
 #
 
 cat /tmp/01_unbound_filters.hostname | sort -T /root/ | uniq | awk -F="" '{ print "0.0.0.0" " " $1}' > /etc/dnsmasq.d/hosts
