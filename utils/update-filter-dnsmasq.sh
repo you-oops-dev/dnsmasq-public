e8220237fb72629c55120563432ebb5377ce0b0f
you-oops-dev[silent] <you-oops-dev[silent]@users.noreply.github.com>
2025-08-28 00:49:09 +0300

    Delete one suite

--- utils/update-filter-dnsmasq.sh
+++ utils/update-filter-dnsmasq.sh
@@ -701,6 +701,7 @@ sed -i '/vsassets.io/d' /tmp/01_unbound_filters.hostname
 sed -i '/vscode-update.azurewebsites.net/d' /tmp/01_unbound_filters.hostname
 sed -i '/ytimg.com/d' /tmp/01_unbound_filters.hostname
 sed -i '/^yastatic.net/d' /tmp/01_unbound_filters.hostname
+sed -i '/cse\.google\.com/d' /tmp/01_unbound_filters.hostname
 ##
 #
 
@@ -763,6 +764,7 @@ sed -i '/youtube\.com\/api\/stats\//d' ${HOME_GITHUB}/ublock_origin_abp.temp
 sed -i '/www\.youtube\.com\/api\/stats\//d' ${HOME_GITHUB}/ublock_origin_abp.temp
 sed -i '/dnsleaktest\.com/d' ${HOME_GITHUB}/ublock_origin_abp.temp
 sed -i '/\/matomo\.js/d' ${HOME_GITHUB}/ublock_origin_abp.temp
+sed -i '/cse\.google\.com/d' ${HOME_GITHUB}/ublock_origin_abp.temp
 #
 sort -T /root/ ${HOME_GITHUB}/ublock_origin_abp.temp | uniq > ${HOME_GITHUB}/ublock_origin_abp.temp.2 && mv -f ${HOME_GITHUB}/ublock_origin_abp.temp.2 ${HOME_GITHUB}/ublock_origin_abp.temp
 
