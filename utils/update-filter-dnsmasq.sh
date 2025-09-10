eba36bf7299e4a1820f284b16ad23d861af6bd45
you-oops-dev[silent] <you-oops-dev[silent]@users.noreply.github.com>
2025-09-10 05:11:30 +0300

    Add new for ublock_origin_abp.txt

--- utils/update-filter-dnsmasq.sh
+++ utils/update-filter-dnsmasq.sh
@@ -787,6 +787,7 @@ sed -i '/||toptracker.ru\^/d' ${HOME_GITHUB}/ublock_origin_abp.temp
 sed -i '/||tapochek.net\^/d' ${HOME_GITHUB}/ublock_origin_abp.temp
 sed -i '/||hidemy.name\^/d' ${HOME_GITHUB}/ublock_origin_abp.temp
 sed -i '/||sourceforge.net\^/d' ${HOME_GITHUB}/ublock_origin_abp.temp
+sed -i '/||sourceforge.io\^/d' ${HOME_GITHUB}/ublock_origin_abp.temp
 #
 sort -T /root/ ${HOME_GITHUB}/ublock_origin_abp.temp | uniq > ${HOME_GITHUB}/ublock_origin_abp.temp.2 && mv -f ${HOME_GITHUB}/ublock_origin_abp.temp.2 ${HOME_GITHUB}/ublock_origin_abp.temp
 
