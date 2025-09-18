7e43be70d2ebff4e889b72908870a93f11a04ca1
you-oops-dev[silent] <you-oops-dev[silent]@users.noreply.github.com>
2025-09-18 18:30:25 +0300

    Add GitHub exclude

--- utils/update-filter-dnsmasq.sh
+++ utils/update-filter-dnsmasq.sh
@@ -711,6 +711,7 @@ sed -i '/irr\.ru/d' /tmp/01_unbound_filters.hostname
 sed -i '/spb\.ru/d' /tmp/01_unbound_filters.hostname
 sed -i '/||spb.ru\^/d' /tmp/01_unbound_filters.hostname
 sed -i '/spb/d' /tmp/01_unbound_filters.hostname
+sed -i '/github.com/d' /tmp/01_unbound_filters.hostname
 #
 
 cat /tmp/01_unbound_filters.hostname | sort -T /root/ | uniq | awk -F="" '{ print "0.0.0.0" " " $1}' > /etc/dnsmasq.d/hosts
@@ -791,6 +792,7 @@ sed -i '/||hidemy.name\^/d' ${HOME_GITHUB}/ublock_origin_abp.temp
 sed -i '/||sourceforge.net\^/d' ${HOME_GITHUB}/ublock_origin_abp.temp
 sed -i '/||sourceforge.io\^/d' ${HOME_GITHUB}/ublock_origin_abp.temp
 sed -i '/||ipv6-test.com\^/d' ${HOME_GITHUB}/ublock_origin_abp.temp
+sed -i '/||github.com\^/d' ${HOME_GITHUB}/ublock_origin_abp.temp
 #
 sort -T /root/ ${HOME_GITHUB}/ublock_origin_abp.temp | uniq > ${HOME_GITHUB}/ublock_origin_abp.temp.2 && mv -f ${HOME_GITHUB}/ublock_origin_abp.temp.2 ${HOME_GITHUB}/ublock_origin_abp.temp
 
